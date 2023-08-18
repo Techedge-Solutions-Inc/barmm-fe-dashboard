407db035094c6e40df8ed2af4fdb0b79d38041cd
AndreLaurio <ict.alaurio@gmail.com>
2023-08-18 13:02:11 +0800

    feat!: title bar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,22 +1,21 @@
 // pages/index.js
 import React from 'react';
 import { Button, Container } from '@mui/material';
+import TitleBar from '@/components/TitleBar/TitleBar';
+import PlusIcon from '@/components/icons/PlusIcon';
 
 const Home = () => {
     return (
-        <Container className="mt-10 text-center">
-            <h1 className="text-4xl font-bold">Welcome to My App</h1>
-            <Button variant="contained" color="primary" className="mt-4">
-                Get Started
-            </Button>
-            <Button variant="contained">Contained</Button>
-            <Button variant="contained" disabled>
-                Disabled
-            </Button>
-            <Button variant="contained" href="#contained-buttons">
-                Link
-            </Button>
-        </Container>
+        <TitleBar
+            title='Transaction History'
+            titleColor='#9E9E9E'
+            policyNumber='87654356782'
+            policyNumberColor='#009865'
+            submitBtn='Claim Insurance'
+            submitBtnIcon={<PlusIcon/>}
+            cancelBtn='Cancel'
+            status='in-review'
+        ></TitleBar>
     );
 };
 
---
+++ src/components/TitleBar/TitleBar.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Button, Chip } from '@mui/material';
+
+interface TitleBarProps {
+    title?: string;
+    titleColor?: string;
+    policyNumber?: string;
+    policyNumberColor?: string;
+    status?: string;
+    cancelBtn?: string;
+    cancelBtnIcon?: React.ReactNode;
+    submitBtn?: string;
+    submitBtnIcon?: React.ReactNode;
+    onCancel?: () => {};
+    onSubmit?: () => {};
+};
+
+const TitleBar:React.FC<TitleBarProps> = ({
+    title, titleColor, policyNumber, policyNumberColor, status, cancelBtn, submitBtn, onCancel, onSubmit, submitBtnIcon, cancelBtnIcon
+}) => {
+    return (
+        <div className='grid grid-cols-12 relative'>
+            <div className='col-span-12 xl:col-span-6 flex'>
+                <div className='text-[18px] md:text-[24px] font-[700]' style={{
+                    color: titleColor
+                }}>
+                    {title}
+                </div>
+                <div className='text-[#9E9E9E] text-[18px] md:text-[24px] font-[700] mx-2'>/</div>
+                <div className='text-[18px] md:text-[24px] font-[700]' style={{
+                    color: policyNumberColor
+                }}>
+                    {policyNumber}
+                </div>
+                <div className='ml-4 -mt-1 md:mt-1 status-option'>
+                    <Chip label={status} variant="outlined" className={status} />
+                </div>
+            </div>
+
+            <div className='col-span-12 xl:col-span-6 xl:justify-end mt-2 xl:mt-0 flex gap-4'>
+                { cancelBtn &&
+                    <Button variant="outlined" disableElevation type='submit' className='w-[200px]'>
+                        {cancelBtnIcon} <span className='ml-2'>{cancelBtn}</span>
+                    </Button>
+                }
+                { submitBtn &&
+                    <Button variant="contained" disableElevation type='submit' className='w-[200px]'>
+                        {submitBtnIcon} <span className='ml-2'>{submitBtn}</span>
+                    </Button>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default TitleBar;
